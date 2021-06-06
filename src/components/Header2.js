24b355d599a92642ff8c60183a60d231639d9835
asfer3031 <asfer3031@gmail.com>
2021-06-06 14:29:14 -0600

    done

--- src/components/Header2.js
+++ src/components/Header2.js
@@ -11,12 +11,13 @@ const Header2 = () => {
                  I am seeking employment as a Web Developer. 
                  I possess an Associate Degree in Information Technology. 
                  Although I am still learning, I can build small projects like this one. 
-                 This project was built using React.Js,Firebase and third party api. 
+                 This project was built using React.Js,Firebase and third party, API. 
                   I am very passionate about my field. I aspire to become a professional Web Developer. 
                   I would be thankful to find a job that would enable me to work within my area of study. 
                   I have gained knowledge through conducting research, enabling practice and motivating myself through a self taught process. 
                   My goal is to aim for more knowledge in different areas of Information Technology. 
                   I would also assist young developers and address their concerns or doubts. 
+                  I am sure that with the right coaching and guidance, I can definately create more complex projects. 
                    Thanks for taking the time to go through this small project. I am open to any comments, suggestions or questions you may have. 
                    Feel free to contact me via email at asfer3031@gmail.com. 
                  </p> 
